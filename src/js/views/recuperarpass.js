18a099c22e5b0c1a315bc2ce51e6ccc66becbe73
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-05-31 19:07:45 +0000

    recuperar pass

--- src/js/views/recuperarpass.js
+++ src/js/views/recuperarpass.js
@@ -5,9 +5,8 @@ import { Context } from "../store/appContext";
 
 import "../../styles/demo.scss";
 
-export const Login = () => {
+export const Recuperarpass = () => {
 	const { store, actions } = useContext(Context);
-
 	return (
 		<div className="container-fluid  px-0 mx-0 mt-5">
 			<div className="card bg-dark text-center text-white">
@@ -18,10 +17,8 @@ export const Login = () => {
 					alt="..."
 				/>
 				<div className="card-img-overlay desenfoque ">
-					<h5 className="card-title mt-4 h2">Sign in</h5>
-					<p className="card-text">¡Es hora de aprender!</p>
+					<h5 className="card-title mt-4 h2">Recuperar password</h5>
 					<div className="row justify-content-center form-group">
-						{/* Email */}
 						<div className="col-5">
 							<p className="text-left">Email address</p>
 							<input
@@ -33,26 +30,13 @@ export const Login = () => {
 						</div>
 					</div>
 					<div className="row justify-content-center form-group">
-						{/* Email */}
 						<div className="col-5">
-							<p className="text-left">Password</p>
-							<input
-								type="password"
-								className="form-control"
-								id="exampleInputEmail1"
-								aria-describedby="emailHelp"
-							/>
-							<Link to="/recuperarpass">
-								<a className="text-right d-block">Olvide mi password</a>
+							<Link to="/">
+								<button className="btn btn-secondary mr-3">Cancelar</button>
 							</Link>
-						</div>
-					</div>
-					<div className="row justify-content-center form-group">
-						<Link to="/">
-							<button className="btn btn-secondary mr-3">Cancelar</button>
-						</Link>
 
-						<button className="btn btn-primary">Aceptar</button>
+							<button className="btn btn-primary">Aceptar</button>
+						</div>
 					</div>
 				</div>
 			</div>

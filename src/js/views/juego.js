a79235d8100662b0fc48bf3d283a8ad5a6e740a0
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-06-10 00:32:08 +0000

    ya se puede jugar

--- src/js/views/juego.js
+++ src/js/views/juego.js
@@ -16,15 +16,13 @@ export const Juego = () => {
 	/* Respuesta va a ser la posicion de la respuesta correcta */
 	const [respuestaC, setRespuesta] = useState();
 	const [resFV, setResFV] = useState("");
-
-	const pauseA = () => {
-		if (!pause) {
-			clearInterval(intervalRef.current);
-		} else {
-			intervalRef.current = setInterval(decreaseNum, 600);
-		}
-		setPause(prev => !prev);
-	};
+	const [position, setPosition] = useState(0);
+	const [ver, setVer] = useState("d-none");
+	const [disableBtn, setDisableBtn] = useState(false);
+	const [btn1, setBtn1] = useState();
+	const [btn2, setBtn2] = useState();
+	const [btn3, setBtn3] = useState();
+	const [puntos, setPuntos] = useState(0);
 
 	/* Cuando llegue a 0 pausar */
 	if (num == 0 || num < 0) {
@@ -56,39 +54,38 @@ export const Juego = () => {
 			clearInterval(intervalRef.current);
 		}
 	};
-
-	const [btn1, setBtn1] = useState();
-	const [btn2, setBtn2] = useState();
-	const [btn3, setBtn3] = useState();
+	/* Play al tiempo */
+	const goTimer = () => {
+		intervalRef.current = setInterval(decreaseNum, 1000);
+	};
 
 	/* hacer un random de 3 y que los cosos eligan cual es la respuesta correcta */
 	const numAleatorio = () => {
 		let respuestaCorrecta = Math.floor(Math.random() * (3 - 1 + 1) + 1);
 		setRespuesta(respuestaCorrecta);
-
 		/* Validamos la respuesta y le damos valor al btn 1 */
-		respuestaCorrecta == 1
-			? setBtn1(store.preguntasYresp[0].respuesta[0].opcion_correcta)
-			: respuestaCorrecta == 2
-				? setBtn1(store.preguntasYresp[0].respuesta[0].opcion_b)
-				: respuestaCorrecta == 3
-					? setBtn1(store.preguntasYresp[0].respuesta[0].opcion_c)
+		respuestaC == 1
+			? setBtn1(store.preguntasYresp[position].respuesta[0].opcion_correcta)
+			: respuestaC == 2
+				? setBtn1(store.preguntasYresp[position].respuesta[0].opcion_b)
+				: respuestaC == 3
+					? setBtn1(store.preguntasYresp[position].respuesta[0].opcion_c)
 					: "";
 		/* Validamos la respuesta 2 le damos valor al btn 2 */
-		respuestaCorrecta == 1
-			? setBtn2(store.preguntasYresp[0].respuesta[0].opcion_c)
-			: respuestaCorrecta == 3
-				? setBtn2(store.preguntasYresp[0].respuesta[0].opcion_b)
-				: respuestaCorrecta == 2
-					? setBtn2(store.preguntasYresp[0].respuesta[0].opcion_correcta)
+		respuestaC == 1
+			? setBtn2(store.preguntasYresp[position].respuesta[0].opcion_c)
+			: respuestaC == 3
+				? setBtn2(store.preguntasYresp[position].respuesta[0].opcion_b)
+				: respuestaC == 2
+					? setBtn2(store.preguntasYresp[position].respuesta[0].opcion_correcta)
 					: "";
 		/* Validamos la respuesta 3 le damos valor al btn 3 */
-		respuestaCorrecta == 1
-			? setBtn3(store.preguntasYresp[0].respuesta[0].opcion_b)
-			: respuestaCorrecta == 2
-				? setBtn3(store.preguntasYresp[0].respuesta[0].opcion_c)
-				: respuestaCorrecta == 3
-					? setBtn3(store.preguntasYresp[0].respuesta[0].opcion_correcta)
+		respuestaC == 1
+			? setBtn3(store.preguntasYresp[position].respuesta[0].opcion_b)
+			: respuestaC == 2
+				? setBtn3(store.preguntasYresp[position].respuesta[0].opcion_c)
+				: respuestaC == 3
+					? setBtn3(store.preguntasYresp[position].respuesta[0].opcion_correcta)
 					: "";
 	};
 
@@ -97,18 +94,39 @@ export const Juego = () => {
 	}, []);
 
 	const respuestaCorrectaVerific = resp => {
-		if (store.preguntasYresp[0].respuesta[0].opcion_correcta === resp) {
+		if (store.preguntasYresp[position].respuesta[0].opcion_correcta === resp) {
 			console.log("Tu respuesta es correcta");
-			setNum(0);
+			handleClick();
 			setResFV("Correcta!");
+			/* Le hacemos un cartel de correcto y mostrar btn siguiente */
 		} else {
-			console.log("NOOOOOO");
+			/* Le hacemos parecer cartel de error y no le sumamos puntos */
+			console.log("Incorrecta D:!");
 			setResFV("Incorrecta D:!");
 			setNum(0);
 		}
+		setVer("d-block");
+		setDisableBtn(true);
 	};
+
+	const aumentarPuntos = () => {
+		setPuntos(puntos + num);
+	};
+
+	const siguientePregunta = () => {
+		let num = position + 1;
+		if (position < 4) {
+			setNum(20);
+			numAleatorio();
+			setPosition(num);
+			setVer("d-none");
+			setDisableBtn(false);
+			goTimer();
+		}
+	};
+
 	/* Cada vez que pase a la siguiente pregunta en un btn siguiente incrementar el [0] por 1  */
-	/* preguntasYresp[0] */
+	/* preguntasYresp[0] position */
 	/* Al hacer click le mandamos el num de la resp que le hicimos click 
         y si concide el num por parametro a respuesttaC= num */
 	return (
@@ -119,16 +137,19 @@ export const Juego = () => {
 						<button className="btn btn-danger">Salir</button>
 					</div>
 					<div className="col-8 text-center text-white">
-						<h1>{store.preguntasYresp[0].preguntado.nombre}</h1>
+						<h1>{store.preguntasYresp[position].preguntado.nombre}</h1>
 					</div>
 					<div className="col-2" />
 					<div className="col-3">
-						<p className="mt-3 p-2 bg-white rounded">Puntos:</p>
-						<p className="mt-3 p-2 bg-white rounded">Partidas 1/5</p>
+						<p className="mt-3 p-2 bg-white rounded">Puntos: {puntos}</p>
+						<p className="mt-3 p-2 bg-white rounded">
+							Partidas {position + 1}
+							/5
+						</p>
 					</div>
 					<div className="col-5 px-0 text-center">
 						<img
-							src={store.preguntasYresp[0].foto_pregunta}
+							src={store.preguntasYresp[position].foto_pregunta}
 							className="figure-img img-fluid rounded w-75"
 							alt="..."
 						/>
@@ -147,7 +168,7 @@ export const Juego = () => {
 				<div className="row justify-content-center">
 					<div className="col-6">
 						<div className="cuadrodepregunta w-100 pt-2 pb-2 text-center">
-							<p className="h4">{store.preguntasYresp[0].preguntas}</p>
+							<p className="h4">{store.preguntasYresp[position].preguntas}</p>
 						</div>
 					</div>
 				</div>
@@ -167,61 +188,103 @@ export const Juego = () => {
 						</div>
 					</div>
 				</div>
-				<div className="row justify-content-center pb-5 mt-2">
-					<div className="col-5">
-						<h3 className="text-white">Tu respuesta es: {resFV} </h3>
+				<div className={ver}>
+					<div className="row justify-content-center">
+						<div className="col-5">
+							<h3 className="text-white">Tu respuesta es: {resFV} </h3>
+						</div>
+						<div className="col-2">
+							<button
+								onClick={() => {
+									aumentarPuntos();
+									siguientePregunta();
+								}}
+								className="btn btn-success">
+								Siguiente ✅
+							</button>
+						</div>
 					</div>
 				</div>
 				<div className="row justify-content-center pb-5 mt-2">
 					<div className="col-5 d-flex justify-content-between">
 						{/* BOTON 1 */}
 						<button
+							disabled={disableBtn}
 							onClick={() => {
-								respuestaCorrectaVerific(btn1);
+								console.log("tu respuesta es" + btn1);
+								respuestaCorrectaVerific(
+									respuestaC == 1
+										? store.preguntasYresp[position].respuesta[0].opcion_correcta
+										: respuestaC == 2
+											? store.preguntasYresp[position].respuesta[0].opcion_b
+											: respuestaC == 3
+												? store.preguntasYresp[position].respuesta[0].opcion_c
+												: ""
+								);
 								handleClick;
 							}}
 							className="btn btn-primary">
 							{respuestaC == 1
-								? store.preguntasYresp[0].respuesta[0].opcion_correcta
+								? store.preguntasYresp[position].respuesta[0].opcion_correcta
 								: respuestaC == 2
-									? store.preguntasYresp[0].respuesta[0].opcion_b
+									? store.preguntasYresp[position].respuesta[0].opcion_b
 									: respuestaC == 3
-										? store.preguntasYresp[0].respuesta[0].opcion_c
+										? store.preguntasYresp[position].respuesta[0].opcion_c
 										: ""}
 						</button>
 						{/* BOTON 2 */}
 
 						<button
+							disabled={disableBtn}
 							onClick={() => {
-								respuestaCorrectaVerific(btn2);
+								console.log("tu respuesta es" + btn2);
+								respuestaCorrectaVerific(
+									respuestaC == 1
+										? store.preguntasYresp[position].respuesta[0].opcion_c
+										: respuestaC == 3
+											? store.preguntasYresp[position].respuesta[0].opcion_b
+											: respuestaC == 2
+												? store.preguntasYresp[position].respuesta[0].opcion_correcta
+												: ""
+								);
 								handleClick;
 							}}
 							className="btn btn-primary">
 							{respuestaC == 1
-								? store.preguntasYresp[0].respuesta[0].opcion_c
+								? store.preguntasYresp[position].respuesta[0].opcion_c
 								: respuestaC == 3
-									? store.preguntasYresp[0].respuesta[0].opcion_b
+									? store.preguntasYresp[position].respuesta[0].opcion_b
 									: respuestaC == 2
-										? store.preguntasYresp[0].respuesta[0].opcion_correcta
+										? store.preguntasYresp[position].respuesta[0].opcion_correcta
 										: ""}
 						</button>
 						{/* BOTON 3 */}
 
 						<button
+							disabled={disableBtn}
 							onClick={() => {
-								respuestaCorrectaVerific(btn3);
+								console.log("tu respuesta es" + btn3);
+								respuestaCorrectaVerific(
+									respuestaC == 1
+										? store.preguntasYresp[position].respuesta[0].opcion_b
+										: respuestaC == 2
+											? store.preguntasYresp[position].respuesta[0].opcion_c
+											: respuestaC == 3
+												? store.preguntasYresp[position].respuesta[0].opcion_correcta
+												: ""
+								);
 								handleClick;
 							}}
 							className="btn btn-primary">
 							{respuestaC == 1
-								? store.preguntasYresp[0].respuesta[0].opcion_b
+								? store.preguntasYresp[position].respuesta[0].opcion_b
 								: respuestaC == 2
-									? store.preguntasYresp[0].respuesta[0].opcion_c
+									? store.preguntasYresp[position].respuesta[0].opcion_c
 									: respuestaC == 3
-										? store.preguntasYresp[0].respuesta[0].opcion_correcta
+										? store.preguntasYresp[position].respuesta[0].opcion_correcta
 										: ""}
 						</button>
-						<h1 className="text-white">{respuestaC}</h1>
+						{/* <h1 className="text-white">{respuestaC}</h1> */}
 					</div>
 				</div>
 			</div>

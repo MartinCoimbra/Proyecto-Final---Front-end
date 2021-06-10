f235400ff4fff3c31ec08fb99f1f50bb25b8da6b
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-06-10 14:54:06 +0000

    map de top listo

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -33,6 +33,7 @@ const injectContext = PassedComponent => {
 			 **/
 			state.actions.getCategorias();
 			state.actions.getPreguntado();
+			state.actions.getTop();
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,

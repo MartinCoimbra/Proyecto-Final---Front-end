1f4032b0a8738abe2fba6141e4280040938bdef8
Maikol Moreira <Niukeitor@hotmail.com>
2021-06-15 01:38:37 +0000

    listo

--- src/js/views/login.js
+++ src/js/views/login.js
@@ -9,62 +9,53 @@ export const Login = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container-fluid  px-0 mx-0 mt-5">
-			<div className="card bg-dark text-center text-white">
-				<img
-					height="600px"
-					src="https://i.pinimg.com/originals/d1/17/88/d11788e6dc8943bc0cd439b21549729d.jpg"
-					className="card-img"
-					alt="..."
-				/>
-				<div className="card-img-overlay desenfoque mt-5">
-					<h5 className="card-title mt-4 h2">Iniciar sesión</h5>
-					<p className="card-text">¡Es hora de aprender!</p>
-					<div className="row justify-content-center form-group">
-						{/* Email */}
-						<div className="col-12 col-sm-5">
-							<p className="text-left">Email</p>
-							<input
-								onChange={actions.loginData}
-								name="email"
-								type="text"
-								className="form-control"
-								id="exampleInputEmail1"
-								aria-describedby="emailHelp"
-							/>
-						</div>
+		<div className="card bg-dark text-center text-white">
+			<div className="card-img-overlay desenfoque mt-5">
+				<h5 className="card-title mt-4 h2">Iniciar sesión</h5>
+				<p className="card-text">¡Es hora de aprender!</p>
+				<div className="row justify-content-center form-group">
+					{/* Email */}
+					<div className="col-12 col-sm-5">
+						<p className="text-left">Email</p>
+						<input
+							onChange={actions.loginData}
+							name="email"
+							type="text"
+							className="form-control"
+							id="exampleInputEmail1"
+							aria-describedby="emailHelp"
+						/>
 					</div>
-					<div className="row justify-content-center form-group">
-						{/* Email */}
-						<div className="col-12 col-sm-5">
-							<p className="text-left">Contraseña</p>
-							<input
-								onChange={actions.loginData}
-								name="password"
-								type="password"
-								className="form-control"
-								id="exampleInputEmail1"
-								aria-describedby="emailHelp"
-							/>
-							<Link to="/recuperarpass">
-								<a className="text-right d-block">Olvide mi password</a>
-							</Link>
-						</div>
-					</div>
-					<div className="row justify-content-center form-group">
-						<Link to="/">
-							<button className="btn btn-secondary mr-3">Cancelar</button>
+				</div>
+				<div className="row justify-content-center form-group">
+					{/* Email */}
+					<div className="col-12 col-sm-5">
+						<p className="text-left">Contraseña</p>
+						<input
+							onChange={actions.loginData}
+							name="password"
+							type="password"
+							className="form-control"
+							id="exampleInputEmail1"
+							aria-describedby="emailHelp"
+						/>
+						<Link to="/recuperarpass">
+							<a className="text-right d-block text-white ">Olvide mi password</a>
 						</Link>
-
-						<button
-							onClick={() => {
-								actions.login();
-							}}
-							className="btn btn-primary">
-							Aceptar
-							{store.logeado == true ? <Redirect to="/" /> : ""}
-						</button>
 					</div>
+				</div>
+				<div className="row justify-content-center form-group">
+					<Link to="/">
+						<button className="btn btn-secondary mr-3">Cancelar</button>
+					</Link>
+					<button
+						onClick={() => {
+							actions.login();
+						}}
+						className="btn btn-primary">
+						Aceptar
+						{store.logeado == true ? <Redirect to="/" /> : ""}
+					</button>
 				</div>
 			</div>
 		</div>
